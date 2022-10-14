e7a94dd90c1d5dcdc4232481354e180545faa561
tecchu11 <agdjmp.ajtt@gmail.com>
2022-10-14 23:00:31 +0900

    sample impl

--- aws_kinesis_stream/src/main.py
+++ aws_kinesis_stream/src/main.py
@@ -3,6 +3,9 @@ from aws_lambda_powertools.utilities.data_classes import (
     KinesisStreamEvent,
     event_source,
 )
+from aws_lambda_powertools.utilities.data_classes.kinesis_stream_event import (
+    KinesisStreamRecord,
+)
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
 logger = Logger()
@@ -11,4 +14,11 @@ logger = Logger()
 @logger.inject_lambda_context()
 @event_source(data_class=KinesisStreamEvent)
 def handler(event: KinesisStreamEvent, context: LambdaContext):
-    print(event)
+    map_result = map(do_something, event.records)
+
+    logger.info(list(map_result))
+
+
+def do_something(record: KinesisStreamRecord) -> str:
+    logger.info(record.kinesis.data_as_text())
+    return record.kinesis.data_as_text()
