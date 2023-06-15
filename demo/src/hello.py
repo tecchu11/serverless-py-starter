1d7ac2c7de7c9fdd1f1eb92b8e40437003073057
tecchu11 <agdjmp.ajtt@gmail.com>
2023-06-16 00:04:07 +0900

    fix

--- demo/src/hello.py
+++ demo/src/hello.py
@@ -1,15 +1,13 @@
 import json
-import os
 
 from aws_lambda_powertools import Logger
 
 logger = Logger()
-message = os.environ["MESSAGE"]
 
 
 @logger.inject_lambda_context(log_event=True)
 def handler(event, context):
-    body = {"message": message, "input": event}
+    body = {"message": "hello", "input": event}
 
     response = {"statusCode": 200, "body": json.dumps(body)}
 
